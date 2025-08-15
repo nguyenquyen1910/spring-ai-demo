af16c796f6627978d27e846239fd21470c0ad731
Nguyen Viet Quyen <jrnguyen14@gmail.com>
2025-08-15 17:30:46 +0700

    feat: system and user prompt

--- src/main/java/com/quyennv/spring_ai_demo/service/ChatService.java
+++ src/main/java/com/quyennv/spring_ai_demo/service/ChatService.java
@@ -1,6 +1,9 @@
 package com.quyennv.spring_ai_demo.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
 
 import com.quyennv.spring_ai_demo.dto.request.ChatRequest;
@@ -14,6 +17,14 @@ public class ChatService {
     }
 
     public String chat(ChatRequest request) {
-        return chatClient.prompt(request.message()).call().content();
+        SystemMessage systemMessage = new SystemMessage("""
+            You are QuyenNV's AI assistant.
+            Your name is Jr.
+                """);
+        UserMessage userMessage = new UserMessage(request.message());
+
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+
+        return chatClient.prompt(prompt).call().content();
     }
 }
