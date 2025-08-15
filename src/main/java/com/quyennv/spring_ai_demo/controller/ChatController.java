224201e5c0c2aac02a49fa0431abf59f875f58e4
Nguyen Viet Quyen <jrnguyen14@gmail.com>
2025-08-15 17:47:59 +0700

    feat: chat with image prompt

--- src/main/java/com/quyennv/spring_ai_demo/controller/ChatController.java
+++ src/main/java/com/quyennv/spring_ai_demo/controller/ChatController.java
@@ -2,7 +2,9 @@ package com.quyennv.spring_ai_demo.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.quyennv.spring_ai_demo.dto.request.ChatRequest;
 import com.quyennv.spring_ai_demo.service.ChatService;
@@ -18,5 +20,11 @@ public class ChatController {
     @PostMapping("/chat")
     String chat(@RequestBody ChatRequest request) {
         return chatService.chat(request);
+    }
+
+    @PostMapping("/chat-with-image")
+    String chatWithImage(@RequestParam("file") MultipartFile file,
+                        @RequestParam("message") String message) {
+        return chatService.chatWithImage(file, message);                    
     }
 }
--- src/main/java/com/quyennv/spring_ai_demo/service/ChatService.java
+++ src/main/java/com/quyennv/spring_ai_demo/service/ChatService.java
@@ -3,8 +3,12 @@ package com.quyennv.spring_ai_demo.service;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.content.Media;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.quyennv.spring_ai_demo.dto.request.ChatRequest;
 
@@ -26,5 +30,28 @@ public class ChatService {
         Prompt prompt = new Prompt(systemMessage, userMessage);
 
         return chatClient.prompt(prompt).call().content();
+    }
+
+    public String chatWithImage(MultipartFile file, String message) {
+        Media media = Media.builder()
+            .mimeType(MimeTypeUtils.parseMimeType(file.getContentType()))
+            .data(file.getResource())
+            .build();
+
+        ChatOptions chatOptions = ChatOptions.builder()
+            .temperature(1D)
+            .build();
+
+
+        return chatClient.prompt()
+            .options(chatOptions)
+            .system("""
+                You are QuyenNV's AI assistant.
+                Your name is Jr.
+                """)
+            .user(promptUserSpec 
+            -> promptUserSpec.media(media)
+            .text(message))
+            .call().content();
     }
 }
